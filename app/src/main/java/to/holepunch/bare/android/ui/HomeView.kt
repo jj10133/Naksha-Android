8e64cbc5c8d03c914fdf4268c54cfe121b501d97
JJ <warmachinesocial@skiff.com>
2025-05-21 14:55:13 -0500

    Added HomeView.kt
    Generally wanted to separate the UI code from the activity

--- app/src/main/java/to/holepunch/bare/android/ui/HomeView.kt
+++ app/src/main/java/to/holepunch/bare/android/ui/HomeView.kt
@@ -1,2 +1,33 @@
 package to.holepunch.bare.android.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+import org.maplibre.android.maps.Style
+import org.ramani.compose.MapLibre
+import to.holepunch.bare.android.viewmodel.HomeViewModel
+
+@Composable
+fun HomeView(homeViewModel: HomeViewModel) {
+    var styleUri by homeViewModel.styleUri
+
+    LaunchedEffect(Unit) {
+        homeViewModel.getMapLink()
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column {
+            MapLibre(
+                modifier = Modifier.fillMaxSize(),
+                styleBuilder = Style.Builder()
+                    .fromUri(styleUri)
+            )
+        }
+    }
+}
--- app/src/main/java/to/holepunch/bare/android/viewmodel/HomeViewModel.kt
+++ app/src/main/java/to/holepunch/bare/android/viewmodel/HomeViewModel.kt
@@ -1,2 +1,30 @@
 package to.holepunch.bare.android.viewmodel
 
+import android.app.Application
+import android.content.Context
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import to.holepunch.bare.android.data_access.ipc.IPCUtils.writeAsync
+import to.holepunch.bare.kit.IPC
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
+
+class HomeViewModel(application: Application, private val ipc: IPC) : ViewModel() {
+    private val appContext: Context = application.applicationContext
+    var styleUri: MutableState<String> = mutableStateOf("")
+
+    suspend fun getMapLink() {
+        val message = "{\"action\": \"requestMapLink\"}"
+
+        val byteBuffer = ByteBuffer.wrap(message.toByteArray(Charset.forName("UTF-8")))
+        ipc.writeAsync(byteBuffer)
+
+        delay(4000)
+
+
+        val mapLink = "file://${appContext.filesDir}/style.json"
+        styleUri.value = mapLink
+    }
+}
