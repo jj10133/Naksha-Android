3484c4c8d4a51129e71f56ecef3d84d92047582e
JJ <>
2025-06-07 22:16:58 -0500

    Fix - import path of Location manager

--- app/src/main/java/com/foss/naksha/android/di/appModule.kt
+++ app/src/main/java/com/foss/naksha/android/di/appModule.kt
@@ -1,10 +1,10 @@
 package com.foss.naksha.android.di
 
 import com.foss.naksha.android.data_access.ipc.IPCProvider
+import com.foss.naksha.android.manager.LocationManager
 import com.foss.naksha.android.viewmodel.HomeViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
-import to.holepunch.bare.android.manager.LocationManager
 
 val appModule = module {
     single { IPCProvider.ipc }
@@ -13,4 +13,3 @@ val appModule = module {
 }
 
 val viewModel = module { viewModel { HomeViewModel(get(), get()) } }
-
